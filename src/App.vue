025404c35b33a6d8c89cad6e892936466a4007a9
Batman <mongez.me@gmail.com>
2023-11-23 11:11:09 +0200

    refactor: encapsulate all node related functions inside one composable

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import { onBeforeUnmount, onMounted, reactive, ref } from 'vue'
 
 import { useCanvas } from '@/canvas'
-import { type NodeType, useNode, useNodeUtils } from '@/node'
+import { useNode, type useNodeType } from '@/node'
 import { type SocketType, useSocket, useSocketUtils } from '@/socket'
 import { useConnection, useConnectionUtils } from '@/connection'
 import { useMouse } from '@/mouse'
@@ -17,13 +17,12 @@ let mouseY = ref(0)
 
 let hasNewConnectionActive = ref(false)
 
-// TODO: This should be an array of selected nodes
-// active dragged node
-const activeNodes: NodeType[] = reactive([])
-const activeSockets: SocketType[] = reactive([])
+// track active/hover nodes
+const activeNodes: useNodeType[] = reactive([])
+const hoverNodes: useNodeType[] = reactive([])
 
-// array of hover sockets
-const hoverNodes: NodeType[] = reactive([])
+// track active/hover sockets
+const activeSockets: SocketType[] = reactive([])
 const hoverSockets: SocketType[] = reactive([])
 
 const DEFAULT_STROKE = 'gray'
@@ -34,7 +33,6 @@ const SELECTED_STROKE = 'yellow'
 // Build the editor nodes, sockets, connections
 //====================================================
 
-const { initNode, isInsideNode, drawNode } = useNodeUtils()
 const { isInsideSocket, drawSocket, connectToNode } = useSocketUtils()
 const { drawConnection } = useConnectionUtils()
 
@@ -56,7 +54,7 @@ const { socket: socket3_i1 } = useSocket({ name: 'x', label: 'x', nodeId: 3 })
 const { socket: socket3_i2 } = useSocket({ name: 'y', label: 'y', nodeId: 3 })
 const { socket: socket3_o1 } = useSocket({ name: 'add', label: 'add', nodeId: 3 })
 
-const { node: node1 } = useNode({
+const node1 = useNode({
   id: 1,
   x: 47,
   y: 285,
@@ -65,7 +63,7 @@ const { node: node1 } = useNode({
   outputSockets: [socket1_o1]
 })
 
-const { node: node2 } = useNode({
+const node2 = useNode({
   id: 2,
   x: 600,
   y: 340,
@@ -74,7 +72,7 @@ const { node: node2 } = useNode({
   outputSockets: [socket2_o1, socket2_o2]
 })
 
-const { node: node3 } = useNode({
+const node3 = useNode({
   id: 3,
   x: 606,
   y: 82,
@@ -85,16 +83,16 @@ const { node: node3 } = useNode({
 
 const { connection: connection1 } = useConnection(
   1,
-  node1.outputSockets[0],
-  node2.inputSockets[0],
+  node1.value.outputSockets[0],
+  node2.value.inputSockets[0],
   mouseX,
   mouseY
 )
 
 const { connection: connection2 } = useConnection(
   2,
-  node1.outputSockets[0],
-  node3.inputSockets[0],
+  node1.value.outputSockets[0],
+  node3.value.inputSockets[0],
   mouseX,
   mouseY
 )
@@ -114,24 +112,24 @@ const { canvas, ctx, renderEditor } = useCanvas()
 
 const repaintEditor = () => {
   renderEditor()
-  allNodes.forEach(initNode)
+  allNodes.forEach((node) => node.init())
   allNodes.forEach((node) => {
-    drawNode(ctx, node)
-    drawSocket(ctx, node)
+    node.draw(ctx)
+    drawSocket(ctx, node.value)
   })
   allConnections.forEach((connection) => drawConnection(ctx, connection))
 }
 
 const activateSelectedNode = () => {
-  activeNodes[0].stroke = SELECTED_STROKE
-  allNodes = allNodes.filter((node) => node.id !== activeNodes[0].id)
+  activeNodes[0].value.stroke = SELECTED_STROKE
+  allNodes = allNodes.filter((node) => node.value.id !== activeNodes[0].value.id)
   allNodes.push(activeNodes[0])
   repaintEditor()
 }
 
 const deactivateSelectedNode = () => {
   if (activeNodes[0]) {
-    activeNodes[0].stroke = DEFAULT_STROKE
+    activeNodes[0].value.stroke = DEFAULT_STROKE
     activeNodes.pop()
   }
   if (activeSockets[0]) {
@@ -147,7 +145,7 @@ const deactivateSelectedNode = () => {
 
 const removeHoverHighlight = () => {
   if (hoverNodes[0]) {
-    hoverNodes[0].stroke = DEFAULT_STROKE
+    hoverNodes[0].value.stroke = DEFAULT_STROKE
     hoverNodes.pop()
   }
   if (hoverSockets[0]) {
@@ -161,7 +159,7 @@ const addHoverHighlight = () => {
   if (hoverSockets[0]) {
     hoverSockets[0].stroke = SELECTED_STROKE
   } else if (hoverNodes[0]) {
-    hoverNodes[0].stroke = SELECTED_STROKE
+    hoverNodes[0].value.stroke = SELECTED_STROKE
   }
   repaintEditor()
 }
@@ -175,17 +173,17 @@ const checkHover = (event: MouseEvent) => {
   const y = event.offsetY
 
   allNodes.forEach((node) => {
-    if (isInsideNode(node, x, y)) {
+    if (node.isMouseInside(x, y)) {
       hoverNodes[0] = node
 
-      node.outputSockets.forEach((socket) => {
-        if (isInsideSocket(node, socket, x, y)) {
+      node.value.outputSockets.forEach((socket) => {
+        if (isInsideSocket(node.value, socket, x, y)) {
           hoverSockets[0] = socket
         }
       })
 
-      node.inputSockets.forEach((socket) => {
-        if (isInsideSocket(node, socket, x, y)) {
+      node.value.inputSockets.forEach((socket) => {
+        if (isInsideSocket(node.value, socket, x, y)) {
           hoverSockets[0] = socket
         }
       })
@@ -227,15 +225,15 @@ const onMouseDown = (event: MouseEvent) => {
   }
 
   allNodes.forEach((node) => {
-    if (isInsideNode(node, x, y)) {
-      node.outputSockets.forEach((socket) => {
-        if (isInsideSocket(node, socket, x, y)) {
+    if (node.isMouseInside(x, y)) {
+      node.value.outputSockets.forEach((socket) => {
+        if (isInsideSocket(node.value, socket, x, y)) {
           activeSockets[0] = socket
         }
       })
 
-      node.inputSockets.forEach((socket) => {
-        if (isInsideSocket(node, socket, x, y)) {
+      node.value.inputSockets.forEach((socket) => {
+        if (isInsideSocket(node.value, socket, x, y)) {
           activeSockets[0] = socket
         }
       })
@@ -249,8 +247,8 @@ const onMouseDown = (event: MouseEvent) => {
       } else {
         isDragging.value = true
         activeNodes[0] = node
-        offsetX.value = x - node.x
-        offsetY.value = y - node.y
+        offsetX.value = x - node.value.x
+        offsetY.value = y - node.value.y
       }
       console.log('[[ Down ]] sockets ', activeSockets[0])
       console.log('[[ Down ]] nodes ', activeNodes[0])
@@ -274,8 +272,8 @@ const onMouseMove = (event: MouseEvent) => {
   const y = event.offsetY
 
   if (isDragging.value) {
-    activeNodes[0].x = event.offsetX - offsetX.value
-    activeNodes[0].y = event.offsetY - offsetY.value
+    activeNodes[0].value.x = event.offsetX - offsetX.value
+    activeNodes[0].value.y = event.offsetY - offsetY.value
     repaintEditor()
   }
 
--- src/node.ts
+++ src/node.ts
@@ -1,136 +1,127 @@
-import {type SocketType} from "@/socket";
-import {type ReactiveVariable} from "vue/macros";
-import * as Vue from "vue-demi";
-import {reactive, type Ref} from "vue";
+import { type SocketType } from '@/socket'
+import { type ReactiveVariable } from 'vue/macros'
+import { reactive, type Ref } from 'vue'
 
 export interface NodeType {
-    id: number;
-    name: string;
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    unit: number;
-    fill: string;
-    stroke: string;
-    strokeWidth: number;
-    borderRadius: number;
-    draggable: boolean;
-    socketRadius: number;
-    inputs: number;
-    outputs: number;
-    socketSpacing: number;
-    headerHeight: number;
-    inputSockets: SocketType[];
-    outputSockets: SocketType[];
+  id: number
+  name: string
+  x: number
+  y: number
+  width: number
+  height: number
+  unit: number
+  fill: string
+  stroke: string
+  strokeWidth: number
+  borderRadius: number
+  draggable: boolean
+  socketRadius: number
+  inputs: number
+  outputs: number
+  socketSpacing: number
+  headerHeight: number
+  inputSockets: SocketType[]
+  outputSockets: SocketType[]
 }
 
-interface useNodeType {
-    node: ReactiveVariable<NodeType>;
-    defaultNode: NodeType;
-}
-
-interface useNodeUtilsType {
-    drawNode: (ctx: Ref<CanvasRenderingContext2D | null>, node: ReactiveVariable<NodeType>) => void;
-    initNode: (node: ReactiveVariable<NodeType>) => void;
-    isInsideNode: (node: NodeType, x: number, y: number) => boolean;
+export interface useNodeType {
+  value: ReactiveVariable<NodeType>
+  defaultValue: NodeType
+  draw: (ctx: Ref<CanvasRenderingContext2D | null>) => void
+  init: () => void
+  isMouseInside: (x: number, y: number) => boolean
 }
 
 export const useNode = (newNode?: Partial<NodeType>): useNodeType => {
-    const defaultNode: NodeType = {
-        id: 1,
-        name: "Sum",
-        x: 1,
-        y: 285,
-        width: 200,
-        height: 200,
-        headerHeight: 40,
-        unit: 50,
-        fill: '#4b4b4b',
-        stroke: 'gray',
-        strokeWidth: 5,
-        borderRadius: 10,
-        draggable: true,
-        socketRadius: 10,
-        inputs: 2,
-        outputs: 5,
-        socketSpacing: 50,
-        inputSockets: [],
-        outputSockets: [],
-    };
+  const defaultValue: NodeType = {
+    id: 1,
+    name: 'Sum',
+    x: 1,
+    y: 285,
+    width: 200,
+    height: 200,
+    headerHeight: 40,
+    unit: 50,
+    fill: '#4b4b4b',
+    stroke: 'gray',
+    strokeWidth: 5,
+    borderRadius: 10,
+    draggable: true,
+    socketRadius: 10,
+    inputs: 2,
+    outputs: 5,
+    socketSpacing: 50,
+    inputSockets: [],
+    outputSockets: []
+  }
 
-    const node: ReactiveVariable<NodeType> = reactive({...defaultNode, ...newNode});
+  const value: ReactiveVariable<NodeType> = reactive({ ...defaultValue, ...newNode })
 
-    return {
-        node,
-        defaultNode,
+  /**
+   * Track if the mouse cursor is inside a node
+   * @param node node to check if mouse is inside
+   * @param x mouse x
+   * @param y mouse y
+   */
+  const isMouseInside = (x: number, y: number) => {
+    return (
+      x >= value.x - value.socketRadius &&
+      x <= value.x + value.width + value.socketRadius &&
+      y >= value.y - value.socketRadius &&
+      y <= value.y + value.height + value.socketRadius
+    )
+  }
+
+  const init = () => {
+    value.outputs = value.outputSockets.length
+    value.inputs = value.inputSockets.length
+
+    // set the height dynamically based on how many sockets
+    const maxSocketsNum = Math.max(value.outputs, value.inputs)
+    value.height = value.unit * (maxSocketsNum + 1) + value.headerHeight
+  }
+
+  const draw = (ctx: Ref<CanvasRenderingContext2D | null>) => {
+    if (!ctx.value) {
+      console.error('You must provide CanvasRenderingContext2D')
+      return
     }
-}
 
-export const useNodeUtils = (): useNodeUtilsType => {
-    /**
-     * Track if the mouse cursor is inside a node
-     * @param node node to check if mouse is inside
-     * @param x mouse x
-     * @param y mouse y
-     */
-    const isInsideNode = (node: NodeType, x: number, y: number) => {
-        return (
-            x >= node.x - node.socketRadius &&
-            x <= node.x + node.width + node.socketRadius &&
-            y >= node.y - node.socketRadius &&
-            y <= node.y + node.height + node.socketRadius
-        );
-    };
+    // draw the node body
+    ctx.value.globalAlpha = 0.93
+    ctx.value.beginPath()
+    ctx.value.fillStyle = value.fill
+    ctx.value.strokeStyle = value.stroke
+    ctx.value.lineWidth = value.strokeWidth
+    ctx.value.roundRect(value.x, value.y, value.width, value.height, value.borderRadius)
+    ctx.value.stroke()
+    ctx.value.fill()
+    ctx.value.closePath()
 
-    const initNode = (node: ReactiveVariable<NodeType>) => {
-        node.outputs = node.outputSockets.length;
-        node.inputs = node.inputSockets.length;
+    // draw the node title
+    ctx.value.textAlign = 'center'
+    ctx.value.font = 'bold 14px Courier New'
+    ctx.value.fillStyle = 'white'
+    ctx.value.fillText(value.name, value.x + value.width / 2, value.y + value.headerHeight / 1.6)
 
-        // set the height dynamically based on how many sockets
-        const maxSocketsNum = Math.max(node.outputs, node.inputs);
-        node.height = node.unit * (maxSocketsNum + 1) + node.headerHeight;
-    }
+    // draw divider
+    ctx.value.beginPath()
+    ctx.value.strokeStyle = '#8c8c8c'
+    ctx.value.lineWidth = 1
+    ctx.value.moveTo(value.x, value.y + value.headerHeight)
+    ctx.value.lineTo(value.x + value.width, value.y + value.headerHeight)
+    ctx.value.stroke()
+    ctx.value.closePath()
 
-    const drawNode = (ctx: Ref<CanvasRenderingContext2D | null>, node: ReactiveVariable<NodeType>) => {
-        if (!ctx.value) {
-            console.error("You must provide CanvasRenderingContext2D");
-            return;
-        }
+    // reset alpha
+    ctx.value.globalAlpha = 1.0
+  }
 
-        // draw the node body
-        ctx.value.globalAlpha = 0.93;
-        ctx.value.beginPath();
-        ctx.value.fillStyle = node.fill;
-        ctx.value.strokeStyle = node.stroke;
-        ctx.value.lineWidth = node.strokeWidth;
-        ctx.value.roundRect(node.x, node.y, node.width, node.height, node.borderRadius);
-        ctx.value.stroke();
-        ctx.value.fill();
-        ctx.value.closePath();
-
-        // draw the node title
-        ctx.value.textAlign = "center";
-        ctx.value.font = 'bold 14px Courier New';
-        ctx.value.fillStyle = "white";
-        ctx.value.fillText(node.name, node.x + node.width / 2, node.y + node.headerHeight / 1.6);
-
-        // draw divider
-        ctx.value.beginPath();
-        ctx.value.strokeStyle = "#8c8c8c";
-        ctx.value.lineWidth = 1;
-        ctx.value.moveTo(node.x, node.y + node.headerHeight);
-        ctx.value.lineTo(node.x + node.width, node.y + node.headerHeight);
-        ctx.value.stroke();
-        ctx.value.closePath();
-
-        // reset alpha
-        ctx.value.globalAlpha = 1.0;
-    };
-
-    return {
-        isInsideNode,
-        initNode,
-        drawNode
-    }
+  return {
+    value,
+    defaultValue,
+    isMouseInside,
+    init,
+    draw
+  }
 }
